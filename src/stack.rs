077bc927def1a9219c1f654c30a058561d9c57fb
birdpersonxo <birdperson.me@gmail.com>
2025-05-17 00:57:05 +0530

    stack start

--- src/stack.rs
+++ src/stack.rs
@@ -1,9 +1,19 @@
 use crate::repository::PackageName;
+use chrono::{DateTime, Utc};
+
+pub struct Stacks{
+    pub stacks: Vec<Stack>
+}
+
 
 #[derive(Debug, Clone)]
 pub struct Stack{
+    pub name: String,
     pub command: String,
     pub package: PackageName,
     pub append: Option<Vec<PackageName>>,
-    pub detach: bool
+    pub detach: bool,
+    pub icon: Option<String>,
+    pub modified_at: Option<DateTime<Utc>>,
+    pub created_at: Option<DateTime<Utc>>,
 }
